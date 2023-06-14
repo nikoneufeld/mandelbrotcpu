45cd0550032c9d8b088dee17ff5f3298bdc34234
Niko Neufeld <nikoneufeld@tutanota.com>
2023-06-14 22:44:54 +0200

    Initial Commit

---
+++ mandelbrotcpu/mandelbrotcpuApp.swift
@@ -0,0 +1,17 @@
+//
+//  mandelbrotcpuApp.swift
+//  mandelbrotcpu
+//
+//  Created by Niko Neufeld on 14.06.23.
+//
+
+import SwiftUI
+
+@main
+struct mandelbrotcpuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
